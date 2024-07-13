cf1f8d93f868e2ddfb2893d814e92c4f5bad4475
khalid fathy <dent.khalidfathy@gmail.com>
2024-07-13 17:03:01 +0300

    added weather_advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,10 +4,7 @@ if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses. ")
 
 elif current_weather == "rainy":
-    print("Don't forget your umbrella and a raincoat. ") 
+    print("Don't forget your umbrella and a raincoat. ")
 
 elif current_weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.
-
-else:
-    print("Sorry, I don't have recommendations for this weather. ")
+    print("Make sure to wear a warm coat and a scarf.")
