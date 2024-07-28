26a8065eaaa259bac70464abde64c40d5305d874
khalid fathy <dent.khalidfathy@gmail.com>
2024-07-28 21:46:41 +0300

    Added BankAccount class and main script for user interaction

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,19 +3,14 @@ class BankAccount:
         self.account_balance = initial_balance
 
     def deposit(self, amount):
-        if amount > 0:
-            self.account_balance += amount
-            print(f"Deposited: ${amount}")
-        else:
-            print("Invalid deposit amount.")
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        if amount > 0 and amount <= self.account_balance:
-            self.account_balance -= amount
+        if self.account_balance >= amount:
+           self.account_balance -= amount
             return True
-        else:
-            print("Insufficient funds.")
+    else:
             return False
-
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
